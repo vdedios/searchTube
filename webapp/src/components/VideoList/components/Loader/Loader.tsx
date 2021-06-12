e55711ccc1234effc596335102fb4a0aa12a3654
Victor Dedios <victor.dedios@outlook.com>
2021-06-12 10:17:17 +0200

    Add Loader styles

--- webapp/src/components/VideoList/components/Loader/Loader.tsx
+++ webapp/src/components/VideoList/components/Loader/Loader.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { withStyles } from '@material-ui/core/styles';
+
+import { LoaderWrapper } from './Loader.styled';
+
+const LoaderIcon = withStyles(() => ({
+  root: {
+    color: '#68BFCD',
+    width: '100%'
+  }
+}))(CircularProgress);
 
 const Loader: React.FC = () => {
-
-  return (
-    <>
-	<p>LOADING...</p>
-    </>
-  );
+    return (
+        <LoaderWrapper>
+            <LoaderIcon/>
+        </LoaderWrapper>
+    );
 }
 
 export default Loader;
