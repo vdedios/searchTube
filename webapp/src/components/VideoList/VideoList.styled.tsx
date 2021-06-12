e76180c71e4e4194ef10e445f429e31dfb4ed51c
Victor Dedios <victor.dedios@outlook.com>
2021-06-12 10:06:28 +0200

    Add VideoList title styles

---
+++ webapp/src/components/VideoList/VideoList.styled.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+    font-family: Helvetica Neue;
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 400;
+    margin: 29px 47px;
+`;
