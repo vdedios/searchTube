692a5741e7d1c0514afa4a3549233a170dbce5c8
Victor Dedios <victor.dedios@outlook.com>
2021-06-12 10:58:25 +0200

    Finish searchbar styles

--- webapp/src/components/SearchBox/SearchBox.tsx
+++ webapp/src/components/SearchBox/SearchBox.tsx
@@ -1,30 +1,34 @@
 import React from 'react';
 
+import MakrwatchLogo from '../../assets/makrwatchLogo.png';
+import { SearchWrapper, Logo, Form, Input, Button, MagnifyingGlass } from './SearchBox.styled';
+
 interface SearchBoxProps {
-  setKeyword: React.Dispatch<React.SetStateAction<string>>;
+    setKeyword: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const SearchBox: React.FC<SearchBoxProps> = ({ setKeyword }: SearchBoxProps) => {
 
-  const search = (event: any) => {
-    event.preventDefault();
-    setKeyword(event.target.elements[0].value)
-  }
+    const search = (event: any) => {
+        event.preventDefault();
+        setKeyword(event.target.elements[0].value)
+    }
 
-  return (
-    <>
-      <form
-        onSubmit={search}
-      >
-        <input
-          placeholder='Search topics on the videos you want to find'
-        />
-        <button>
-          GO
-        </button>
-      </form>
-    </>
-  );
+    return (
+        <SearchWrapper>
+            <Logo src={MakrwatchLogo} />
+            <Form
+                onSubmit={search}
+            >
+                <Input
+                    placeholder='Search topics on the videos you want to find'
+                />
+                <Button>
+                    <MagnifyingGlass/>
+                </Button>
+            </Form>
+        </SearchWrapper>
+    );
 }
 
 export default SearchBox;
